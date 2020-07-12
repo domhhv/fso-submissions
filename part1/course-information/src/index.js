f5f45de5501e67eab84834f9946a024be690cc71
Dominik Hryshaiev <dgrishajev@gmail.com>
2020-07-12 18:14:51 +0300

    add submission for exercise 1.5 of part1

--- part1/course-information/src/index.js
+++ part1/course-information/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = ({ name }) => <h1>{name}</h1>
 
 const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
 
@@ -16,27 +16,29 @@ const Content = ({ parts: [first, second, third] }) => (
 const Total = ({ parts: [first, second, third] }) => <p>Number of exercises {first.exercises + second.exercises + third.exercises}</p>
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     )
 }
