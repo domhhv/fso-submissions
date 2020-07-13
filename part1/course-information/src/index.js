97baaf85e94207e16909814f8859f189105805c3
Dominik Hryshaiev <dgrishajev@gmail.com>
2020-07-14 02:54:20 +0300

    add submission for exercise 1.6 of part1

--- part1/course-information/src/index.js
+++ part1/course-information/src/index.js
@@ -6,41 +6,41 @@ const Header = ({ name }) => <h1>{name}</h1>
 const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
 
 const Content = ({ parts: [first, second, third] }) => (
-    <>
-        <Part {...first} />
-        <Part {...second} />
-        <Part {...third} />
-    </>
+	<>
+		<Part {...first} />
+		<Part {...second} />
+		<Part {...third} />
+	</>
 )
 
 const Total = ({ parts: [first, second, third] }) => <p>Number of exercises {first.exercises + second.exercises + third.exercises}</p>
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7
+			},
+			{
+				name: 'State of a component',
+				exercises: 14
+			}
+		]
+	}
 
-    return (
-        <div>
-            <Header name={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    )
+	return (
+		<div>
+			<Header name={course.name} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
+		</div>
+	)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
