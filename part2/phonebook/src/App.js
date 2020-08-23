a7b4b726a576ad819f3e4c15c0de59fc47c5f205
Dominik Hryshaiev <dgrishajev@gmail.com>
2020-08-23 16:19:29 +0300

    add submission for exercises 2.18-2.19 of part2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -10,22 +10,43 @@ import {
 import PersonsForm from './components/PersonsForm'
 import Control from './components/Control'
 import Persons from './components/Persons'
+import { Notification, NOTIFICATION_STATES } from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [criteria, setCriteria] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState('')
+  const [notificationState, setNotificationState] = useState('')
+  const [cleanUpMessageTimeoutId, setCleanUpMessageTimeoutId] = useState(null)
 
   const clearFields = () => {
     setNewName('')
     setNewNumber('')
   }
 
+  const clearMessage = () => {
+    setNotificationMessage('')
+    setNotificationState('')
+  }
+
+  const scheduleMessageCleanUp = delay => window.setTimeout(clearMessage, delay)
+
+  const notify = ({ message, state }) => {
+    window.clearTimeout(cleanUpMessageTimeoutId)
+    setNotificationMessage(message)
+    setNotificationState(state)
+    setCleanUpMessageTimeoutId(
+      scheduleMessageCleanUp(5000)
+    )
+  }
   const resolvePersons = items => setPersons(items)
 
   const fetchPersons = () => {
     fetchAll().then(resolvePersons)
+
+    return window.clearTimeout(cleanUpMessageTimeoutId)
   }
 
   useEffect(fetchPersons, [])
@@ -52,7 +73,7 @@ const App = () => {
       const shouldReplace = window.confirm(`${newName} has already been added to the phonebook, replace the old number with the new one?`)
 
       if (shouldReplace) {
-        const { id } = existingPerson
+        const { id, name } = existingPerson
 
         return update(id, { ...existingPerson, number: newNumber }).then(updatedPerson => {
           const accumulatePersons = (acc, person) => {
@@ -67,6 +88,18 @@ const App = () => {
 
           clearFields()
           setPersons(updatedPersons)
+          setNotificationMessage(`Updated ${updatedPerson.name}'s number`)
+          setNotificationState(NOTIFICATION_STATES.SUCCESS)
+          scheduleMessageCleanUp(5000)
+        }).catch(error => {
+          console.error(error)
+          notify({
+            message: `Data of ${name} has already been deleted from the server`,
+            state: NOTIFICATION_STATES.ERROR,
+          })
+          const matchId = person => person.id !== id
+          const filteredPersons = persons.filter(matchId)
+          setPersons(filteredPersons)
         })
       }
 
@@ -76,18 +109,33 @@ const App = () => {
     create(newName, newNumber).then(newPerson => {
       clearFields()
       setPersons([...persons, newPerson])
+      notify({
+        message: `Added ${newPerson.name}`,
+        state: NOTIFICATION_STATES.SUCCESS,
+      })
     })
   }
 
   const deletePerson = (personId, personName) => {
     if (window.confirm(`Delete ${personName}?`)) {
       remove(personId)
+        .then(() => {
+          notify({
+            message: `${personName} has been deleted`,
+            state: NOTIFICATION_STATES.SUCCESS,
+          })
+        })
         .catch(error => {
-          window.alert(`${personName} is already deleted`)
           console.error(error)
+          notify({
+            message: `${personName} is already deleted`,
+            state: NOTIFICATION_STATES.ERROR,
+          })
         })
         .finally(() => {
-          setPersons(persons.filter(({ id }) => id !== personId))
+          const matchId = ({ id }) => id !== personId
+          const filteredPersons = persons.filter(matchId)
+          setPersons(filteredPersons)
         })
     }
   }
@@ -95,6 +143,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notificationMessage} state={notificationState} />
       <PersonsForm data={controlsData} handler={preventify(addPerson)} />
       <h2>Numbers</h2>
       <Control label="filter shown with" value={criteria} handler={withValue(setCriteria)} />
